8020cdc851abc158f8b0f4dddddc14452330c05e
divya <dhivyakesavulu1999@gmail.com>
2022-05-28 17:43:22 +0530

    employee list program

---
+++ SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/dhivya/EmployeeList.java
@@ -0,0 +1,41 @@
+package com.codexbox.springboot.app.dhivya;
+
+import java.util.Scanner;
+public class EmployeeList {
+    public String name;
+    public Integer Id;
+    public String address;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Integer getId() {
+        return Id;
+    }
+    public void setId(Integer id) {
+        Id = id;
+    }
+    public String getAddress() {
+        return address;
+    }
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+public  void detils(){
+    System.out.println("---------------------------------------------------");
+    System.out.println(name + " " + Id  + " " +  address);
+    //System.out.println(Id);
+    //System.out.println(address);
+
+}
+
+
+
+
+
+    }
+
