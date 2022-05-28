ac7450aa95cbe6b793250485e79709153506cc94
CodexBox <vidya.darnacodexbox.com>
2022-05-28 17:27:17 +0530

    EmployeeList details

--- SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/SpringBootCodexBoxJava2022Batch1Application.java
+++ SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/SpringBootCodexBoxJava2022Batch1Application.java
@@ -1,15 +1,34 @@
 package com.codexbox.springboot.app;
 
-import org.springframework.boot.SpringApplication;
+import com.codexbox.springboot.app.vidyadarna.Employee;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.naming.Name;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class SpringBootCodexBoxJava2022Batch1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootCodexBoxJava2022Batch1Application.class, args);
-		LinkedListExample example = new LinkedListExample();
-		System.out.println(example.loadMyList());
-	}
 
+	public static void main(String[] args) {
+
+
+		//SpringApplication.run(SpringBootCodexBoxJava2022Batch1Application.class, args);
+		List<Employee> employeeList = new ArrayList<>();
+
+		Scanner s = new Scanner(System.in);
+		for (int i = 1; i<=2; i++) {
+			System.out.println("enter the employee name");
+			String EmployeeName = s.next();
+			System.out.println("enter the employee id");
+			int EmployeeId = s.nextInt();
+			System.out.println(" enter employee address");
+			String EmployeeAddress = s.next();
+			Employee employee = new Employee(EmployeeName, EmployeeId, EmployeeAddress);
+			employeeList.add(employee);
+		}
+
+	}
 }
