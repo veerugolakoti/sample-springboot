d3a8144d43700c67257563536183d5f79f3ec7aa
CodexBox <vasu.manginipelli@codexbox.com>
2022-05-28 18:14:59 +0530

    assesment employee details

---
+++ SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/vasu/sample/Employee.java
@@ -0,0 +1,13 @@
+package com.codexbox.springboot.app.vasu.sample;
+public class Employee {
+    private String Name;
+    private Integer Id;
+    private String Address;
+
+    public Employee(String Name,Integer Id,String Address){
+        this.Name=Name;
+        this.Id=Id;
+        this.Address=Address;
+
+    }
+}
---
+++ SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/vasu/sample/Main.java
@@ -0,0 +1,32 @@
+package com.codexbox.springboot.app.vasu.sample;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+    @SpringBootApplication
+    public class Main {
+
+        public static void main(String[] args) {
+            List<Employee> employeeList=new ArrayList<>();
+            Scanner obj=new Scanner(System.in);
+            for (int i = 1; i <=29 ; i++) {
+                System.out.println("Employee details" +i+ "Details");
+
+                System.out.println("Enter employee name: ");
+                String name =obj.next();
+                System.out.println("Enter employee Id: ");
+                int Id=obj.nextInt();
+                System.out.println("Enter employee Address: ");
+                String address =obj.next();
+
+                Employee employee=new Employee(name,Id,address);
+                employeeList.add(employee);
+            }
+            System.out.println("my total employees:" +employeeList.size());
+        }
+
+    }
+
+
