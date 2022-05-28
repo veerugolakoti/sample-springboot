8094a9cfe47bd48dd8d6262ca5a72cd418d30b34
CodexBox <swathigajjela1998@gmail.com>
2022-05-28 17:00:14 +0530

    object added to array list...

---
+++ SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/swathi/assesment/mainmethod/Main.java
@@ -0,0 +1,35 @@
+package com.codexbox.springboot.app.swathi.assesment.mainmethod;
+
+import com.codexbox.springboot.app.swathi.assesment.EmployeeDetails;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // SpringApplication.run(SpringBootCodexBoxJava2022Batch1Application.class, args);
+
+        List<EmployeeDetails> employees = new ArrayList<>();
+
+        for (int i = 1; i < 29; i++) {
+            Scanner object = new Scanner(System.in);
+            System.out.println("employee name");
+            String EmployeeName = object.next();
+            System.out.println("employee id");
+            String EmployeeId = object.next();
+            System.out.println("employee address");
+            String employeeAddress = object.next();
+            EmployeeDetails employee = new EmployeeDetails(EmployeeName, EmployeeId, employeeAddress);
+           employees.add(employee);
+
+            //employee.printEmployeeDetails();
+        }
+        for (int i = 1; i < 29; i++) {
+           // System.out.println(Employee.get(i).printEmployeeDetails());
+            employees.get(i).printEmployeeDetails();
+        }
+    }}
+
+
+
