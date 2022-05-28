ea9b95908ebd0f0b532336109c7592fcd3a104de
rameshthota123 <thotaramesh768@gmail.com>
2022-05-28 17:08:28 +0530

    create employe list

---
+++ SpringBootCodexBoxJava2022Batch1/src/main/java/assment2/EmployeDetails.java
@@ -0,0 +1,15 @@
+package assment2;
+
+import java.util.LinkedList;
+import java.util.Scanner;
+public class EmployeDetails {
+    private  String name;
+    private  Integer id;
+    private  String address;
+
+    public EmployeDetails(String name, Integer id, String address) {
+        this.name = name;
+        this.id = id;
+        this.address = address;
+    }
+}
--- SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/SpringBootCodexBoxJava2022Batch1Application.java
+++ SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/SpringBootCodexBoxJava2022Batch1Application.java
@@ -1,15 +1,31 @@
 package com.codexbox.springboot.app;
 
+import assment2.EmployeDetails;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 @SpringBootApplication
 public class SpringBootCodexBoxJava2022Batch1Application {
-
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootCodexBoxJava2022Batch1Application.class, args);
-		LinkedListExample example = new LinkedListExample();
-		System.out.println(example.loadMyList());
-	}
+		List<EmployeDetails> employelist = new ArrayList<>();
+		Scanner sc = new Scanner(System.in);
+		for (int i = 1; i <= 29; i++) {
+			System.out.println(" Enter the Employes"+i+"Details");
+			System.out.println("Enter The Employe name ");
+			String name = sc.next();
+			System.out.println("Enter The id");
+			Integer id = sc.nextInt();
+			System.out.println("Enter The address");
+			String address = sc.next();
+			EmployeDetails employeDetails = new EmployeDetails("sasi",1,"ong");
+			employelist.add(employeDetails);
+			System.out.println("My total employes details :"+employelist.size());
 
+
+		}
+	}
 }
