b7a2a71fc4d5c8ad1cd52f2f7ddda63d5c1e445e
CodexBox <junaid.mohammed@codexbox.com>
2022-05-28 16:56:29 +0530

    Added the object to the arraylist

---
+++ SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/junaidspringbootproject/springboot/Employee.java
@@ -0,0 +1,14 @@
+package com.codexbox.springboot.app.junaidspringbootproject.springboot;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Employee
+{
+    public static void main(String[] args) {
+        Scanner inp = new Scanner(System.in);
+        ArrayList list = new ArrayList();
+        list.add(inp.nextLine());
+        System.out.println(list);
+    }
+}
