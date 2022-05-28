3ad6e5581d135ea680833d89df2ff7b99baec142
CodexBox <teja.aravindkumar@gmail.com>
2022-05-28 16:53:45 +0530

    Assessment commit

--- SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/SpringBootCodexBoxJava2022Batch1Application.java
+++ SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/SpringBootCodexBoxJava2022Batch1Application.java
@@ -1,15 +1,48 @@
 package com.codexbox.springboot.app;
 
+import com.codexbox.springboot.app.TejaAravind.Employee;
+import com.codexbox.springboot.app.TejaAravind.ListOfEmployees;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class SpringBootCodexBoxJava2022Batch1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootCodexBoxJava2022Batch1Application.class, args);
-		LinkedListExample example = new LinkedListExample();
-		System.out.println(example.loadMyList());
+		List<Employee> list = new ArrayList<>();
+		for (int i = 0; i < 2; i++) {
+			Employee employee = new Employee();
+			System.out.println("Enter the Employee "+(i+1)+" details : ");
+			employee.setDetails();
+			list.add(employee);
+		}
+
+		for (Employee emp : list) {
+			emp.display();
+		}
+
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter the name you want to remove : ");
+		String str = in.nextLine();
+		Employee emp= new Employee();
+		for (int i = 0; i < list.size();i++) {
+			if (list.get(i).getName().equalsIgnoreCase(str)){
+				System.out.println("Enter new employee details");
+				emp.setDetails();
+				list.set(i,emp);
+				break;
+			}
+		}
+
+		for (Employee emp1 : list) {
+			emp1.display();
+		}
+
 	}
 
 }
