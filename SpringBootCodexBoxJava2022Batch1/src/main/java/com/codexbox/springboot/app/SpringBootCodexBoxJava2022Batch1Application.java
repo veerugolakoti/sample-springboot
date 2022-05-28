4eaa1ab291e007005868a21898bc831be2eecfca
CodexBox <harshamulisetti56@gmail.com>
2022-05-28 17:16:14 +0530

    employee list program using arraylist

--- SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/SpringBootCodexBoxJava2022Batch1Application.java
+++ SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/SpringBootCodexBoxJava2022Batch1Application.java
@@ -1,15 +1,40 @@
 package com.codexbox.springboot.app;
 
+import com.codexbox.springboot.app.HarshaDeepthi.EmployeeDetails;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 @SpringBootApplication
 public class SpringBootCodexBoxJava2022Batch1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootCodexBoxJava2022Batch1Application.class, args);
-		LinkedListExample example = new LinkedListExample();
-		System.out.println(example.loadMyList());
+		//SpringApplication.run(SpringBootCodexBoxJava2022Batch1Application.class, args);
+	//	LinkedListExample example = new LinkedListExample();
+		//System.out.println(example.loadMyList());
+
+
+		List<EmployeeDetails> employeeDetailsList = new ArrayList<>();
+		Scanner scanner=new Scanner((System.in));
+		for(int i=1; i<=29; i++){
+			System.out.println("enter emp id:");
+			Integer empId = scanner.nextInt();
+
+			System.out.println("enter emp name:");
+			String empName = scanner.next();
+
+			System.out.println("enter the emp address:");
+			String empadd = scanner.next();
+			EmployeeDetails empDetl =new EmployeeDetails(empId,empName,empadd);
+
+			employeeDetailsList.add(empDetl);
+
+		}
+		System.out.println("details of employee:"+employeeDetailsList.size());
+
 	}
 
 }
