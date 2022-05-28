6a38aad5e3d761f30af2c78cf22ee1cb3a0a3466
CodexBox <ramdurga.penaganti@codexbox.com>
2022-05-28 17:36:41 +0530

    intial spring project commit

--- SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/SpringBootCodexBoxJava2022Batch1Application.java
+++ SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/SpringBootCodexBoxJava2022Batch1Application.java
@@ -1,15 +1,36 @@
 package com.codexbox.springboot.app;
 
-import org.springframework.boot.SpringApplication;
+import com.codexbox.springboot.app.ramdurgaprasad.Employee;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class SpringBootCodexBoxJava2022Batch1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootCodexBoxJava2022Batch1Application.class, args);
-		LinkedListExample example = new LinkedListExample();
-		System.out.println(example.loadMyList());
+		List<Employee> employeeList=new ArrayList<Employee>();
+		System.out.println("enter the details");
+		for(int i=1;i<=3;i++) {
+			System.out.println("the employee"+i+" name,id,address:");
+			Employee emp = new Employee();
+			employeeList.add(emp);
+			} int count =1;
+		for (Employee list :employeeList) {
+
+			System.out.println("the name,id,address of employee "+count++);
+			System.out.print(list.getName() + ",");
+			System.out.print(list.getEmpid()+",");
+			System.out.print(list.getCity());
+			System.out.println(" ");
+
+		}
+
+
+		}
+
+
 	}
 
-}
+
