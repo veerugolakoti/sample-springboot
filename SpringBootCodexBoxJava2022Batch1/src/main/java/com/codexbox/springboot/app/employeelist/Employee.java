1f979962450d2f1e1c7fdf5f6a2ada05d0a32fd2
SASIGUNTURI <sasigunturi143@gmail.com>
2022-05-28 17:38:11 +0530

    add the employee list

---
+++ SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/employeelist/Employee.java
@@ -0,0 +1,15 @@
+package com.codexbox.springboot.app.employeelist;
+
+public class Employee {
+    private Integer empid;
+    private String empname;
+   private String address;
+  public Employee (){
+      this.empid = empid;
+      this.empname = empname;
+      this.address = address;
+
+   }
+}
+
+
