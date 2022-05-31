03dbd06b1ca966c5665cea4a43908b550cf0a0cc
jadduleelaganesh <jadduleelaganesh@gmail.com>
2022-05-31 16:49:48 +0530

    Changes are done

--- SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/LinkedListExample.java
+++ SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/LinkedListExample.java
@@ -5,11 +5,26 @@ import java.util.List;
 
 public class LinkedListExample {
 
-   public List<String> loadMyList() {
+   public void m1() {
        List<String> list = new LinkedList<>();
-       list.add("Name1");
+
+       list. add("gun");
        list.add("Name2");
-       return list;
-    }
+       list.add("Name3");
+       list.add("Name4");
+       list.add("Name14");
+       list.add("Name24");
+       list.add("Name18");
+       list.add("Name23");
+        list.add("Name18");
+       list.add("Name26");
+       list.add("civil");
+       list.set(6,"jil");
+        list.set(5,"try");
+       System.out.println(list);
+       System.out.println(list.size());
+
+
+   }
 
 }
