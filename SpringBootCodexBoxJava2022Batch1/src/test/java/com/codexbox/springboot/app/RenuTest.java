8a1363d6bae8ca85fcd171af01ddd2dc9c1cb38c
CodexBox <renuka.velidhineni@codexbox.com>
2022-06-01 16:59:12 +0530

    map example

--- SpringBootCodexBoxJava2022Batch1/src/test/java/com/codexbox/springboot/app/RenuTest.java
+++ SpringBootCodexBoxJava2022Batch1/src/test/java/com/codexbox/springboot/app/RenuTest.java
@@ -4,10 +4,7 @@ import com.codexbox.springboot.app.renuProjects.assessments.EmployeeDetails;
 import com.codexbox.springboot.app.renuProjects.collections.ExampleOfQueue;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Stack;
+import java.util.*;
 
 public class RenuTest {
 
@@ -60,8 +57,71 @@ public class RenuTest {
         System.out.println(charset.contains('c'));
         ArrayList<Character> chararray=new ArrayList<>(charset);
         System.out.println(chararray);
+        System.out.println(chararray.get(5));
+
     }
 
+
+ @Test
+    void exampleOfLinkedHashSet(){
+     LinkedHashSet<String> names=new LinkedHashSet<>();
+        names.add("apple");
+        names.add("cat");
+        names.add("dog");
+        names.add("1");
+        names.add("elephant");
+        names.add("book");
+        names.add("@,3");
+     System.out.println(names);
+     System.out.println(names.hashCode());
+     System.out.println(names.size());
+     TreeSet<String> setnames=new TreeSet<>();
+     setnames.addAll(names);
+     System.out.println(setnames);
+     //System.out.println(setnames.descendingIterator());
+     System.out.println(setnames.first());
+     System.out.println(setnames.pollFirst());
+     System.out.println(setnames.pollLast());
+ }
+
+  @Test
+    void mapExample(){
+        Map<String,Integer> mapname=new HashMap<>();
+        mapname.put("jansi",60);
+        mapname.put("ravali",65);
+        mapname.put("divya",71);
+        mapname.put("swathi",69);
+        mapname.put("jansi",61);
+      System.out.println(mapname);
+      System.out.println(mapname.size());
+      System.out.println(mapname.keySet());
+      System.out.println(mapname.get("jansi"));
+
+      EmployeeDetails emp1=new EmployeeDetails("harsha",90,"hyd");
+      EmployeeDetails emp2=new EmployeeDetails("mounika",72,"ongole");
+      EmployeeDetails emp3=new EmployeeDetails("shiva",95,"hyd");
+
+      Map<String,EmployeeDetails> empdetails=new HashMap<>();
+      empdetails.put("employee1" , emp1);
+      empdetails.put("employee2" , emp2);
+      empdetails.put("employee3" , emp3);
+
+      System.out.println(empdetails);
+      Collection<EmployeeDetails> empcollection=empdetails.values();
+
+      for (EmployeeDetails emp:empcollection) {
+          System.out.println(emp.getEmpid());
+      }
+
+      EmployeeDetails emp = empdetails.get("employee2");
+      System.out.println(emp.getEmpname());
+
+
+      for (Map.Entry<String, EmployeeDetails> entry : empdetails.entrySet()) {
+          System.out.println(entry.getKey( ) +" "+entry.getValue().getEmpname()+" "+entry.getValue().getEmpid()+" "+entry.getValue().getAddress());
+      }
+
+  }
 
 
 
