46189168c58c2fed9bd18c2d9994d86f2c7a1e80
example1 <kalyankadamanchi5253@gmail.com>
2022-06-06 18:34:32 +0530

    assessment

--- SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/SpringBootCodexBoxJava2022Batch1Application.java
+++ SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/SpringBootCodexBoxJava2022Batch1Application.java
@@ -1,16 +1,9 @@
 package com.codexbox.springboot.app;
 
-import assessment3.Employee;
-import assessment4.Student;
+import assessment.StudentGrade;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 @SpringBootApplication
 public class SpringBootCodexBoxJava2022Batch1Application {
-
 	public static void main(String[] args) {
 		//int count = 1;
 	/*	List<Employee> empList = new ArrayList<>();
@@ -29,28 +22,7 @@ public class SpringBootCodexBoxJava2022Batch1Application {
 		System.out.println("My total employees : " + empList.size());
 	}*/
 
-		List<Student> studentList=new ArrayList<>();
-		Scanner sc=new Scanner(System.in);
-		for (int i=1;i<3;i++) {
-			System.out.println("enter student " + i + " details ");
-			System.out.print("enter student name : ");
-			String name = sc.next();
-			System.out.print("enter student id : ");
-			int id = sc.nextInt();
-			System.out.print("enter student address : ");
-			String address = sc.next();
-			System.out.print("enter student marks : ");
-			int marks = sc.nextInt();
-
-			float percentage =(marks/100)*100;
-
-			Student student = new Student(name, id, address, marks,percentage);
-			studentList.add(student);
-		}
-		for (Student s:studentList) {
-			System.out.println(s.getSname()+" : " +s.getSid()+ " : " +s.getSaddress()+ " : " +s.getSmarks()+" : "+s.getPercentage()+ " : "+s.getGrade());
-		}
-
+		StudentGrade studentGrade=new StudentGrade();
+		studentGrade.studentGrades();
 	}
-
 }
--- SpringBootCodexBoxJava2022Batch1/src/test/java/com/codexbox/springboot/app/Kalyan_Testing.java
+++ SpringBootCodexBoxJava2022Batch1/src/test/java/com/codexbox/springboot/app/Kalyan_Testing.java
@@ -3,6 +3,7 @@ package com.codexbox.springboot.app;
 import collections.MapsExamples;
 import collections.TreeSetExample;
 import duplicateNames.DuplicateExample;
+import enumerate.Directions;
 import org.junit.jupiter.api.Test;
 import rateofInterest.InterestExample;
 
@@ -19,6 +20,13 @@ public class Kalyan_Testing {
     @Test
     public void testMapExample(){
         MapsExamples.setMap();
+    }
+    @Test
+    public void testDirections(){
+      Directions arr[]=Directions.values();
+        for (Directions d:arr) {
+            System.out.println(d+" at position : "+d.ordinal());
+        }
     }
 
 }
