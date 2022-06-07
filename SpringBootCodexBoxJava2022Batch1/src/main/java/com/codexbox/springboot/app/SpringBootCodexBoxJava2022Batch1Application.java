7f982871121da84a1d3a964eeb266cd554607895
SASIGUNTURI <sasigunturi143@gmail.com>
2022-06-07 19:05:07 +0530

    commit to Fileinputouputstream

--- SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/SpringBootCodexBoxJava2022Batch1Application.java
+++ SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/SpringBootCodexBoxJava2022Batch1Application.java
@@ -1,15 +1,14 @@
 package com.codexbox.springboot.app;
 
-import org.springframework.boot.SpringApplication;
+import com.codexbox.springboot.app.fileinputstream.FileInputStreamExample;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.IOException;
 
 @SpringBootApplication
 public class SpringBootCodexBoxJava2022Batch1Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootCodexBoxJava2022Batch1Application.class, args);
-		LinkedListExample example = new LinkedListExample();
-		System.out.println(example.loadMyList());
+	public static void main(String[] args) throws IOException {
+		FileInputStreamExample fis = new FileInputStreamExample();
+		fis.filecounting();
 	}
-
 }
