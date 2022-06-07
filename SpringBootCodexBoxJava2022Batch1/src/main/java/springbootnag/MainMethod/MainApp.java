0e6ff7db54d8e5a9a5f053c49bed6a1735cdab5b
CodexBox <nagarjunapdtm123>
2022-06-07 18:39:48 +0530

    files commited

--- SpringBootCodexBoxJava2022Batch1/src/main/java/springbootnag/MainMethod/MainApp.java
+++ SpringBootCodexBoxJava2022Batch1/src/main/java/springbootnag/MainMethod/MainApp.java
@@ -1,7 +1,6 @@
 package springbootnag.MainMethod;
 
 import com.codexbox.springboot.app.SpringBootCodexBoxJava2022Batch1Application;
-import org.junit.Test;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import springboot.nag.AddEmployees;
