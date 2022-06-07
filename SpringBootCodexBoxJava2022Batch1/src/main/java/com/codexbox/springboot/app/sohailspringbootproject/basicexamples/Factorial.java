082153d838dddbe513de8d0d27ae92caeb0b6a61
sohail1212 <sohail1212786@gmail.com>
2022-06-07 09:34:58 +0530

    Factorial Example

--- SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/sohailspringbootproject/basicexamples/Factorial.java
+++ SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/sohailspringbootproject/basicexamples/Factorial.java
@@ -1,6 +1,19 @@
 package com.codexbox.springboot.app.sohailspringbootproject.basicexamples;
 
-public class Factorial {
-    int fact=0;
+import java.util.Scanner;
 
-}
+public class Factorial {
+    public void factorialMethod(){
+        int x= 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the number of factorial you want : ");
+        x=scanner.nextInt();
+        for(int i = 0 ; i<x;i++) {
+
+        }
+            System.out.println();
+        }
+
+    }
+
+
--- SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/sohailspringbootproject/main/MainMethod.java
+++ SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/sohailspringbootproject/main/MainMethod.java
@@ -1,6 +1,6 @@
 package com.codexbox.springboot.app.sohailspringbootproject.main;
 
-import com.codexbox.springboot.app.sohailspringbootproject.examples.Factorial;
+import com.codexbox.springboot.app.sohailspringbootproject.basicexamples.Factorial;
 
 public class MainMethod {
     public static void main(String [] args ){
