16fd323828669119f78b80cbad67f76576d6f7c8
jadduleelaganesh <jadduleelaganesh@gmail.com>
2022-06-07 16:06:30 +0530

    Student Assessment modified

--- SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/SpringBootCodexBoxJava2022Batch1Application.java
+++ SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/SpringBootCodexBoxJava2022Batch1Application.java
@@ -1,10 +1,9 @@
 package com.codexbox.springboot.app;
 
-import com.codexbox.springboot.app.Veeru.Collections.StudentGradeAssessment;
-import com.codexbox.springboot.app.Veeru.employee.Employee;
+import com.codexbox.springboot.app.ganesh.Scanner.Assessment.Students;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -12,9 +11,9 @@ import java.util.Scanner;
 public class SpringBootCodexBoxJava2022Batch1Application {
 
 	public static void main(String[] args) {
-		StudentGradeAssessment assessment4 = new StudentGradeAssessment();
-		assessment4.studentAndTheirGrades();
-	} /*{
+//		StudentGradeAssessment assessment4 = new StudentGradeAssessment();
+//		assessment4.studentAndTheirGrades();
+	 /*{
 		//int count = 1;
 		List<Employee> empList = new ArrayList<>();
 		Scanner scanner = new Scanner(System.in);
@@ -33,5 +32,47 @@ public class SpringBootCodexBoxJava2022Batch1Application {
 		}
 		System.out.println("My total employees : " + empList.size());
 	}*/
+
+
+//		Students students = new Students();
+//		System.out.println(students.calculategrade(560));
+//
+//
+
+		/////
+//		IOFileHandling ioFileHandling =new IOFileHandling();
+//		ioFileHandling.fileoperation();
+
+///////
+		{
+			Scanner s = new Scanner(System.in);
+			System.out.println("**********Enter the total no students************");
+			int totalstudents = s.nextInt();
+			List<Students> order = new LinkedList<>();
+
+			for (int i = 1; i <= totalstudents; i++) {
+				System.out.println("==========Enter the Students "+i+" Details============");
+				System.out.println("Enter the Students numbeer");
+				int number = s.nextInt();
+				System.out.println("Enter the Students name");
+				String name= s.next();
+				System.out.println("Enter the Students address");
+				String  address =s.next();
+				System.out.println("Enter the Students marks");
+				Integer marks= s.nextInt();
+				Students  students = new Students(number,name,address,marks);
+				order.add(students);
+
+			}
+			for (Students nostudents:order) {
+				System.out.println(nostudents );
+
+			}
+
+		}
+
+
+
+	}
 
 }
--- SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/ganesh/Scanner/Assessment/Students.java
+++ SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/ganesh/Scanner/Assessment/Students.java
@@ -1,10 +1,15 @@
 package com.codexbox.springboot.app.ganesh.Scanner.Assessment;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Students {
-  private int Studentnumber;
-  private String Studentname,Studentaddress;
+    private int Studentnumber;
+    private String Studentname,Studentaddress;
     private String result;
-    private Float marks,percentage;
+    private Float  percentage;
+    private  int marks;
 
     private int getStudentnumber() {
         return Studentnumber;
@@ -29,10 +34,9 @@ public class Students {
     }
 
 
-    public Float getMarks() {
+    public int getMarks() {
         return marks;
     }
-
 
 
 
@@ -42,56 +46,50 @@ public class Students {
 
 
 
-    public Students(int Studentnumber, String Studentname, String Studentaddress, Float marks ) {
+   public Students(int Studentnumber, String Studentname, String Studentaddress, int marks ) {
 
 
-        this.Studentnumber = Studentnumber;
-        this.Studentname = Studentname;
-        this.Studentaddress = Studentaddress;
-        this.marks = marks;
-
-    }
+       this.Studentnumber = Studentnumber;
+       this.Studentname = Studentname;
+       this.Studentaddress = Studentaddress;
+       this.marks = marks;
+   }
 //
-//    {
-//        List<Students> order = new LinkedList<>();
-//        Scanner s = new Scanner(System.in);
-//        for (int i = 1; i <= 6; i++) {
-//            System.out.println("==========Enter the Students "+i+" Details============");
-//            System.out.println("Enter the Students numbeer");
-//            Studentnumber = s.nextInt();
-//            System.out.println("Enter the Students name");
-//        Studentname  =s.next();
-//            System.out.println("Enter the Students address");
-//            Studentaddress   =s.next();
-//           System.out.println("Enter the Students marks");
-//             marks = s.nextFloat();
-//        }
-//        Students st = new Students(Studentnumber,Studentname,Studentaddress,marks);
-//        order.add(Students);
+//    public Students (){
+//
 //    }
-public void calculate(int marks){
+//
+
+
+
+
+
+    public String calculategrade(int marks){
         percentage= (float) marks/6; //type casting int - float
 
- if (percentage >90)
-{
-    result="Grade1";
-}
-
-    else if(percentage <90 && percentage>60)
-    {
-        result ="Grade2";
-    }
-    else if(percentage <60 && percentage>35)
-    {
-        result ="Grade3";
-    } else if (percentage<35)
-    {
-    result ="fail";
+        if (percentage >90)
+        {
+            result="Grade1";
         }
-    System.out.println(" percentage ="+percentage);
-    System.out.println("Result= "+result);
-}
 
+        else if(percentage <90 && percentage>60)
+        {
+            result ="Grade2";
+        }
+        else if(percentage <60 && percentage>35)
+        {
+            result ="Grade3";
+        }
+        else
+        {
+            result ="fail";
+        }
+
+        System.out.println(" percentage ="+percentage);
+        System.out.println("Result= "+result);
+
+         return result;
+    }
 }
 
 
