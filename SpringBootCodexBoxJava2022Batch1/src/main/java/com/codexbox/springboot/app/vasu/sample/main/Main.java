74fae0fa39241298d4e1a5950f5c0af8a79d9409
CodexBox <vasu.manginipelli@codexbox.com>
2022-06-08 18:39:40 +0530

    assessment4 and enum,input and outut streams

--- SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/vasu/sample/main/Main.java
+++ SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/vasu/sample/main/Main.java
@@ -1,3 +1,4 @@
+
 package com.codexbox.springboot.app.vasu.sample.main;
 
 import com.codexbox.springboot.app.vasu.sample.collections.StackExample;
@@ -5,9 +6,13 @@ import com.codexbox.springboot.app.vasu.sample.collections.StackExample1;
 
 public class Main {
     public static void main(String[] args) {
-        /*StackExample stackExample=new StackExample();
-        stackExample.stackoperations();*/
+
+StackExample stackExample=new StackExample();
+        stackExample.stackoperations();
+
         StackExample1 stackExample1=new StackExample1();
         stackExample1.stackoperations();
     }
 }
+
+
--- SpringBootCodexBoxJava2022Batch1/src/test/java/com/codexbox/springboot/app/InputOutputStreams.java
+++ SpringBootCodexBoxJava2022Batch1/src/test/java/com/codexbox/springboot/app/InputOutputStreams.java
@@ -5,15 +5,23 @@ import java.io.*;
 public class InputOutputStreams {
     public void fileInputOutputoperations() throws IOException {
         InputStream inputStream=null;
+        InputStream inputStream1=null;
         OutputStream outputStream=null;
         try{
             inputStream=new FileInputStream("pqr.txt");
+            inputStream1=new FileInputStream("mno.txt");
             outputStream=new FileOutputStream("vaasu.txt");
-            System.out.println(inputStream.read());
+
             int asci=0;
             while (asci!=-1){
                 asci=inputStream.read();
                 outputStream.write(asci);
+            }
+           // System.out.println(inputStream.read());
+            int asci1=0;
+            while (asci1!=-1){
+                asci1=inputStream1.read();
+                outputStream.write(asci1);
             }
         } catch (FileNotFoundException e) {
             System.out.println("File Not Found Exception");
@@ -22,6 +30,8 @@ public class InputOutputStreams {
         }
         finally {
             inputStream.close();
+            inputStream1.close();
+            outputStream.close();
         }
 
     }
