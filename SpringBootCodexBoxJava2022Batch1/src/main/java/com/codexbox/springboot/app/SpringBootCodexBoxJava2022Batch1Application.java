cb1fd646a171f0491e842ba3809e2a01aa9d7d83
CodexBox <jhansi.gurubadu@codexbox.com>
2022-06-13 19:19:39 +0530

    threading examples

--- SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/SpringBootCodexBoxJava2022Batch1Application.java
+++ SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/SpringBootCodexBoxJava2022Batch1Application.java
@@ -6,6 +6,7 @@ import com.codexbox.springboot.app.assesment3.StudentClass;
 import com.codexbox.springboot.app.jhansiproject.collections.StackExample;
 import com.codexbox.springboot.app.jhansiproject.enumeration.EnumerationWeekday;
 import com.codexbox.springboot.app.jhansiproject.enumeration.Month;
+import com.codexbox.springboot.app.jhansiproject.multithreading.ThreadExample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -16,6 +17,15 @@ import java.util.Collections;
 public class SpringBootCodexBoxJava2022Batch1Application {
 
 	public static void main(String[] args) {
+		ThreadExample thread=new ThreadExample();
+		thread.multithreading();
+		thread.run();
+		thread.run();
+		ThreadExample thread1=new ThreadExample();
+		thread.run();
+		thread.run();
+
+
 		/*SpringApplication.run(SpringBootCodexBoxJava2022Batch1Application.class, args);
 		LinkedListExample example = new LinkedListExample();
 		System.out.println(example.loadMyList());
