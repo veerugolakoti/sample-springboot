30a5f257c18f490ed0b630affb90db2a8a7a5fc5
shiva1938 <shivashivam17@gmail.com>
2022-06-13 19:19:39 +0530

    Threads

--- SpringBootCodexBoxJava2022Batch1/src/main/java/MainMethod/MainApp.java
+++ SpringBootCodexBoxJava2022Batch1/src/main/java/MainMethod/MainApp.java
@@ -5,6 +5,8 @@ import Enumeration.EnumerationEx;
 import Enumeration.EnumerationWeekDay;
 import FilesOperations.*;
 import FilesOperationsChallenge.*;
+import MultiThreadeing.MyThread;
+import MultiThreadeing.MyThreadExample;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -97,7 +99,37 @@ public class MainApp {
        /* BufferedReadMultipeLines bufferLines = new BufferedReadMultipeLines();
         bufferLines.multipleLines();*/
 
-        SeqenceStreams sequence = new SeqenceStreams();
-        sequence.sequencestream();
+        /*SeqenceStreams sequence = new SeqenceStreams();
+        sequence.sequencestream();*/
+
+        /*MyThread thread = new MyThread();
+        MyThread thread2 = new MyThread();
+        MyThread thread3 = new MyThread();
+        thread.run();
+        thread2.run();
+        System.out.println();
+        System.out.println(thread.getId());
+        System.out.println(thread.getName());
+        System.out.println();
+        System.out.println(thread2.getId());
+        System.out.println(thread2.getName());
+        System.out.println();
+        System.out.println(thread3.getId());
+        System.out.println(thread3.getName());
+        System.out.println();
+        Thread  threads = new Thread();
+        Thread threads1 = new Thread();
+        System.out.println(threads.getId());
+        System.out.println(threads.getName());
+        System.out.println();
+        System.out.println(threads1.getId());
+        System.out.println(threads1.getName());*/
+
+
+        for(int i = 0; i < 10; i++) {
+            MyThreadExample threadex = new MyThreadExample();
+            threadex.start();
+        }
+        MyThreadExample threadx = new MyThreadExample();
     }
 }
