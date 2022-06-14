1e7aecd7506166663d892dec20190ec6b16407b6
CodexBox <vamshi.caprico@gmail.com>
2022-06-14 19:15:34 +0530

    Tested Thread Methods

--- SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/vamshi/MainSpringBootApi.java
+++ SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/vamshi/MainSpringBootApi.java
@@ -1,5 +1,6 @@
 package com.codexbox.springboot.app.vamshi;
 
+import com.codexbox.springboot.app.vamshi.multithreading.MyRunnableSample;
 import com.codexbox.springboot.app.vamshi.multithreading.MyThreadSample;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,10 +9,49 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MainSpringBootApi {
     public static void main(String[] args) {
         MyThreadSample myThreadSample = new MyThreadSample();
+        System.out.println(myThreadSample.getState());
+        myThreadSample.start();
+       // System.out.println(myThreadSample.getState());
+        try {
+            myThreadSample.join();
+            System.out.println("check joining ");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+       // System.out.println(myThreadSample.getState());
+        try {
+            Thread.sleep(10000);
+            System.out.println("check : sleep activated");
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(Thread.currentThread().getPriority());
+        System.out.println(myThreadSample.getState());
+
+
+
+
+      /*
+        //runnable
+        MyRunnableSample myRunnableSample = new MyRunnableSample();
+        myRunnableSample.run();
+        try {
+            Thread.sleep(1000l);
+            // myRunnableSample.notify();
+            // myRunnableSample.wait();
+            System.out.println("check sleep works ");
+            System.out.println(Thread.currentThread().getState());
+        } catch (InterruptedException e) {
+        }*/
+
+
+
+
+
+        /*MyThreadSample myThreadSample = new MyThreadSample();
         int a = 11, b=44, c=a*b ;
         System.out.println("total >>>"+c);
 
-        myThreadSample.run();
         myThreadSample.start();
 
         myThreadSample.testThread();
@@ -24,7 +64,7 @@ public class MainSpringBootApi {
 
         MyThreadSample myThreadSample1 = new MyThreadSample();
         System.out.println("check5  "+myThreadSample1.getId());
-        System.out.println(myThreadSample1.getName());
+        System.out.println(myThreadSample1.getName());*/
 
 
 
--- SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/vamshi/multithreading/MyThreadSample.java
+++ SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/vamshi/multithreading/MyThreadSample.java
@@ -1,15 +1,15 @@
 package com.codexbox.springboot.app.vamshi.multithreading;
 
-public class MyThreadSample extends Thread{
+public class MyThreadSample extends Thread implements Runnable{
     @Override
     public void run() {
         for (int i=1;i<=5;i++){
             System.out.println("check number >>>"+i);
         }
-            System.out.println("check1 "+Thread.currentThread().getId());
+        System.out.println("check1 "+Thread.currentThread().getState());
     }
 
     public void testThread(){
-        System.out.println("check2 "+Thread.currentThread().getName());
+        System.out.println("check2 "+Thread.currentThread().getId());
     }
 }
