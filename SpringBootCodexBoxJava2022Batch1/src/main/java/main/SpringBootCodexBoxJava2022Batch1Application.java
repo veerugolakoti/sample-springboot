735e72ff3d40e252cb0dfd678a7d6efd48b62927
example1 <kalyankadamanchi5253@gmail.com>
2022-06-14 19:13:45 +0530

    threads example2

--- SpringBootCodexBoxJava2022Batch1/src/main/java/main/SpringBootCodexBoxJava2022Batch1Application.java
+++ SpringBootCodexBoxJava2022Batch1/src/main/java/main/SpringBootCodexBoxJava2022Batch1Application.java
@@ -3,6 +3,7 @@ package main;
 import ioStreams.ArrayStream;
 import ioStreams.BufferedStream;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import threads.RunableExample;
 import threads.ThreadExample;
 
 import java.io.IOException;
@@ -11,7 +12,30 @@ import java.util.ArrayList;
 @SpringBootApplication
 public class SpringBootCodexBoxJava2022Batch1Application {
 	public static void main(String[] args) {
-		ThreadExample threadExample=new ThreadExample();
+		ThreadExample thread=new ThreadExample();
+		System.out.println(thread.getState());
+		thread.start();
+		try {
+			Thread.sleep(200);
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+		System.out.println(thread.getState());
+		try {
+			thread.join();
+			System.out.println("iam joining my new thread");
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+   		System.out.println(thread.getState());
+	}
+	}
+	/*	RunableExample runableExample =new RunableExample();
+		Thread thread=new Thread(runableExample);
+		System.out.println(thread.getState());
+	}}*/
+
+/*		ThreadExample threadExample=new ThreadExample();
 		threadExample.run();
 
 		ThreadExample threadExample1=new ThreadExample();
@@ -22,7 +46,7 @@ public class SpringBootCodexBoxJava2022Batch1Application {
 		System.out.println(threadExample2.getName());
 		System.out.println(threadExample2.getId());
 	}
-}
+}*/
 		/*ArrayStream as=new ArrayStream();
 		try {
 			as.myArrayStream();
