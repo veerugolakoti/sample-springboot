051c49eded1b2725f256271373313d445e351bd3
shiva1938 <shivashivam17@gmail.com>
2022-06-14 19:00:07 +0530

    Threads and priority threads

--- SpringBootCodexBoxJava2022Batch1/src/main/java/MainMethod/MainApp.java
+++ SpringBootCodexBoxJava2022Batch1/src/main/java/MainMethod/MainApp.java
@@ -5,12 +5,13 @@ import Enumeration.EnumerationEx;
 import Enumeration.EnumerationWeekDay;
 import FilesOperations.*;
 import FilesOperationsChallenge.*;
-import MultiThreadeing.MyThread;
-import MultiThreadeing.MyThreadExample;
+import MultiThreadeing.*;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.PriorityQueue;
+
+import static MultiThreadeing.ThreadPriorityEx2.addition;
 
 public class MainApp {
     public static void main(String[] args) throws IOException {
@@ -126,10 +127,58 @@ public class MainApp {
         System.out.println(threads1.getName());*/
 
 
-        for(int i = 0; i < 10; i++) {
+        /*for(int i = 0; i < 10; i++) {
             MyThreadExample threadex = new MyThreadExample();
             threadex.start();
         }
-        MyThreadExample threadx = new MyThreadExample();
+        MyThreadExample threadx = new MyThreadExample();*/
+
+        /*MyThreadExample2 thread2 = new MyThreadExample2();
+        thread2.start();
+        System.out.println("State of the thread: " + thread2.getState());
+        try {
+            Thread.sleep(800);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        for(int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }*/
+
+        /*ThreadPriorityExamples tpriority1 = new ThreadPriorityExamples();
+        ThreadPriorityExamples tpriority2 = new ThreadPriorityExamples();
+        ThreadPriorityExamples tpriority3 = new ThreadPriorityExamples();
+
+        System.out.println("Priority of thread: " + tpriority1.getPriority());
+        System.out.println("Priority of thread: " + tpriority2.getPriority());
+        System.out.println("Priority of thread: " + tpriority3.getPriority());
+
+        tpriority1.setPriority(3);
+        tpriority2.setPriority(2);
+        tpriority3.setPriority(6);
+
+        System.out.println("Priority of thread: " + tpriority1.getPriority());
+        System.out.println("Priority of thread: " + tpriority2.getPriority());
+        System.out.println("Priority of thread: " + tpriority3.getPriority());
+
+        tpriority1.run();
+        System.out.println(tpriority1.getState());
+        tpriority1.start();
+        System.out.println(tpriority1.getState());*/
+
+        ThreadPriorityEx2 tpriority22 = new ThreadPriorityEx2();
+        ThreadPriorityEx2 tpriority33 = new ThreadPriorityEx2();
+        ThreadPriorityEx2 tpriority44 = new ThreadPriorityEx2();
+        tpriority22.setPriority(3);
+        tpriority33.setPriority(8);
+        tpriority44.setPriority(5);
+
+        System.out.println(tpriority22.getPriority());
+        System.out.println(tpriority33.getPriority());
+        System.out.println(tpriority44.getPriority());
+
+
+
+
     }
 }
