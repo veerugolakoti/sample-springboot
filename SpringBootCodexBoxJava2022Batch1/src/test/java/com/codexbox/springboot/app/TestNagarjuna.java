4921f01718b4b5635aaa21f091ecb2b57f821f84
CodexBox <nagarjunapdtm123>
2022-06-14 19:00:11 +0530

    Multithreading commited

--- SpringBootCodexBoxJava2022Batch1/src/test/java/com/codexbox/springboot/app/TestNagarjuna.java
+++ SpringBootCodexBoxJava2022Batch1/src/test/java/com/codexbox/springboot/app/TestNagarjuna.java
@@ -3,6 +3,8 @@ package com.codexbox.springboot.app;
 import Enumeration.Size;
 import Enumeration.Week;
 import FileOperations.*;
+import Multithreading.Mythread;
+import Multithreading.Mythread1;
 import Multithreading.ThreadExample;
 import org.junit.jupiter.api.Test;
 
@@ -56,8 +58,8 @@ public class TestNagarjuna {
         ba.inputOutputOperations();*/
        /* ByteArrayOutStream bao=new ByteArrayOutStream();
         bao.inputOutputOperations();*/
-   public void  ownThread() {
-       ThreadExample te = new ThreadExample();
+    public void ownThread() {
+      /* ThreadExample te = new ThreadExample();
         System.out.println("my thread id "+Thread.currentThread().getId());
         System.out.println("my thread name "+Thread.currentThread().getName());
 
@@ -75,15 +77,61 @@ public class TestNagarjuna {
         Thread thread=new Thread();
         thread.run();
         System.out.println("my thread id "+thread.getId());
-        System.out.println("my thread id  "+thread.getName());
+        System.out.println("my thread id  "+thread.getName());*/
 
-        System.out.println(thread.getState());
+       /* System.out.println(thread.getState());
         System.out.println(thread.getContextClassLoader());
         System.out.println(thread.getPriority());
         System.out.println(thread.getThreadGroup());
         System.out.println(thread.getStackTrace());
         System.out.println(thread.toString());
-        System.out.println(thread.isInterrupted());
-   }
+        System.out.println(thread.isInterrupted());*/
+//----------------------Dated on 14-06-22------------------------
+        /*Mythread mythread=new Mythread();
+        mythread.run();
+
+
+        Thread thread=new Thread();
+        System.out.println(thread.getState());
+        thread.start();
+        System.out.println(thread.getState());
+
+
+Thread thread1=new Thread();
+        System.out.println(thread1.getState());
+        thread1.start();
+        System.out.println(thread1.getState());
+  try {
+            thread.join();
+            thread.sleep(20000l);
+
+        } catch (InterruptedException e) {
+
+        }
+
+        System.out.println("I did not sleep");*/
+        Mythread1 mythread1 = new Mythread1();
+        mythread1.run();
+        System.out.println("this thread is inside my class");
+        Thread thread = new Thread();
+        System.out.println(thread.getState());
+        thread.start();
+        System.out.println(thread.getState());
+
+        System.out.println("my thread1");
+        System.out.println("my thread2");
+        System.out.println("my thread3");
+        System.out.println(thread.getState());
+        System.out.println(thread.getPriority());
+
+        try {
+
+             Thread.sleep(575l);
+        } catch (InterruptedException e) {
+
+            System.out.println("i am awake");
+
+        }
+    }
 }
 
