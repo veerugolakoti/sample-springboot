067e5094e2a2dc25898b01f6db827111ae9daddb
siva <sivarayapaneni31@gmail.com>
2022-06-15 19:05:43 +0530

    Practise on multithreading with wait & notify method

--- SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/multithreading/MainThread.java
+++ SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/multithreading/MainThread.java
@@ -1,15 +1,24 @@
 package com.codexbox.springboot.app.multithreading;
 
 public class MainThread {
-    public static void main(String[] args) {
-        MyThread myThread = new MyThread();
-     myThread.mythreadMethod();
-        System.out.println(Thread.currentThread().getId());
-        System.out.println(Thread.currentThread().getName());
-        Thread thread = new Thread();
-        System.out.println(thread.getId());
-        System.out.println(thread.getName());
-        }
+    public static void main(String[] args)  {
+          MyThread myThread = new MyThread();
+          myThread.start();
+           synchronized (myThread){
+    try {
+        System.out.println("print something");
+        myThread.wait();
+    } catch (InterruptedException e) {
+        throw new RuntimeException(e);
     }
+    System.out.println("total is :"+ myThread.total);
+}
+
+            }
+        }
+
+
+
+
 
 
--- SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/multithreading/MyThread.java
+++ SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/multithreading/MyThread.java
@@ -1,26 +1,23 @@
 package com.codexbox.springboot.app.multithreading;
 
 public class MyThread extends Thread{
-       public void mythreadMethod(){
-        MyThread myThread = new MyThread();
-        System.out.println("Thread id:"+ myThread.getId());
-        System.out.println("Thread name:"+myThread.getName());
+    int total;
+    @Override
+    public void run() {
+       // System.out.println("i am running");
+synchronized (this){
+    for (int i = 0; i<5; i++){
+        total += i;
 
-        MyThread myThread1 = new MyThread();
-        System.out.println("Thread id:"+ myThread1.getId());
-        System.out.println("Thread name:"+ myThread1.getName());
+    }
+    notify();
+}
 
-        System.out.println("Thread id:" + Thread.currentThread().getId());
-        System.out.println("Thread name:"+ Thread.currentThread().getName());
-
-        Thread thread = new Thread();
-        System.out.println(thread.getId());
-        System.out.println(thread.getName());
-
+    }
     }
 
 
 
-        }
+
 
 
