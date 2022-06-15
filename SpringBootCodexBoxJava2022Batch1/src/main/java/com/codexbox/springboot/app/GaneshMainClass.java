3cbb73fc808537b95bd68f83e288833c3601fce0
jadduleelaganesh <jadduleelaganesh@gmail.com>
2022-06-15 19:35:46 +0530

    Thread & Runnable Example

--- SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/GaneshMainClass.java
+++ SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/GaneshMainClass.java
@@ -1,7 +1,7 @@
 package com.codexbox.springboot.app;
 
+import com.codexbox.springboot.app.ganesh.multithreading.RunnableExample;
 import com.codexbox.springboot.app.ganesh.multithreading.ThreadExample;
-import com.codexbox.springboot.app.ganesh.multithreading.ThreadExample1;
 
 public class GaneshMainClass {
     public static void main(String[] args) {
@@ -30,23 +30,102 @@ synchronized (example){
 }
 */
 
-   // ******************************//
+        // ******************************//
 
-        ThreadExample1 te1 =new ThreadExample1();
+       /* ThreadExample1 te1 = new ThreadExample1();
 
-        System.out.println( te1.getState());
+        System.out.println(te1.getState());
         te1.start();
         System.out.println(te1.getState());
-synchronized (te1){
-    System.out.println("main thread");
-    try {
-        te1.wait(3000);
-        System.out.println("After the main thread");
-    } catch (InterruptedException e) {
-        throw new RuntimeException(e);
-    }
-}
+        synchronized (te1) {
+            System.out.println("main thread");
+            try {
+                te1.wait(3000);
+                System.out.println("After the main thread");
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
         System.out.println(te1.getState());
+///////////////////////
+
+        Display1 d1 = new Display1();
+        Thread thread = new Thread(d1);
+        thread.start();
+d1.run();*/
+
+///////*****
+
+      /*  //A class
+
+
+        ThreadExample t1 = new ThreadExample();
+        Thread thread0 = new Thread();
+        thread0.start();
+        // c class
+        RunnableExample runnableExample = new RunnableExample();
+        Thread thread = new Thread(runnableExample);
+        thread.start();
+
+        //B class
+
+        ThreadExample1 te1 = new ThreadExample1(runnableExample);
+        Thread thread1 = new Thread();
+        thread1.start();
+*/
+
+
+
+
+
+//////****
+
+        RunnableExample runnableExample = new RunnableExample();
+        Thread t1= new Thread(runnableExample);
+        t1.start();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+Thread t2 = new Thread(runnableExample);
+        t2.start();
+        try {
+            Thread.sleep(7000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+Thread t3= new Thread(runnableExample);
+        t3.start();
+        try {
+            Thread.sleep(9000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        //**
+        ThreadExample t4 = new ThreadExample();
+        t4.start();
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+ThreadExample t5 = new ThreadExample();
+        t5.start();
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        ThreadExample t6 = new ThreadExample();
+        t6.start();
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
-}
+    }
--- SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/ganesh/multithreading/ThreadExample.java
+++ SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/ganesh/multithreading/ThreadExample.java
@@ -1,9 +1,19 @@
 package com.codexbox.springboot.app.ganesh.multithreading;
 
 public class ThreadExample extends Thread{
-    public   void run()
-    {
-        for (int i = 1; i < 5; i++) {
+    int counter=0;
+
+    public  void run() {
+
+        counter++;
+        System.out.println("Implements from thread=" + counter);
+
+    }
+
+}
+
+
+      /*  for (int i = 1; i < 5; i++) {
             System.out.println("My nuber is="+i);
         }
         try {
@@ -11,9 +21,5 @@ public class ThreadExample extends Thread{
             System.out.println(getState());
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
-        }
+        }*/
 
-
-    }
-
-}
