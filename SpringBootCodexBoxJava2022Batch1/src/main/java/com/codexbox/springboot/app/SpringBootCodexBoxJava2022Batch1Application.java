20df23a3c63012b1588a516092a7d9bcba41b18f
CodexBox <vasu.manginipelli@codexbox.com>
2022-06-15 19:30:34 +0530

    streams,theads concepts

--- SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/SpringBootCodexBoxJava2022Batch1Application.java
+++ SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/SpringBootCodexBoxJava2022Batch1Application.java
@@ -1,8 +1,13 @@
 package com.codexbox.springboot.app;
 
+import com.codexbox.springboot.app.vasu.sample.assesment.Employee;
 import com.codexbox.springboot.app.vasu.sample.multithreading.MyThread;
 import com.codexbox.springboot.app.vasu.sample.multithreading.Thread1;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 import static java.lang.Integer.sum;
 
@@ -10,67 +15,8 @@ import static java.lang.Integer.sum;
 public class SpringBootCodexBoxJava2022Batch1Application {
 
 	public static void main(String[] args) {
-		System.out.println("Iam inside main method");
-		int x=50;
-		int y=40;
-		int sum=sum(x,y);
-		System.out.println(sum);
-		MyThread myThread=new MyThread();
-		Thread1 thread1=new Thread1();
-		System.out.println(thread1.isAlive());
-		System.out.println(myThread.isAlive());
-		myThread.setPriority(10);
-		thread1.setPriority(1);
-		System.out.println(thread1.getPriority());
-		thread1.start();
-		myThread.start();
-		System.out.println(myThread.getState());
-		System.out.println(myThread.getPriority());
-		try {
-			Thread.sleep(10000);
-		} catch (InterruptedException e) {
-			throw new RuntimeException(e);
-		}
-			System.out.println("sleep method");
 
-
-
-		/*Thread1 thread1=new Thread1();
-		thread1.run();
-
-		try {
-			Thread.sleep(10000);
-		} catch (InterruptedException e) {
-			throw new RuntimeException(e);
-		}
-		System.out.println("time up");
-
-		MyThread Thread=new MyThread();
-		Thread.start();
-		try {
-			Thread.join(10000);
-		} catch (InterruptedException e) {
-			throw new RuntimeException(e);
-		}
-		System.out.println("its lunch time");
-		// Thread concept..
-
-		System.out.println(Thread.currentThread().getId());
-		System.out.println(Thread.currentThread().getName());
-		MyThread myThread1=new MyThread();
-		myThread1.run();
-		System.out.println(myThread1.getId());
-		System.out.println(myThread1.getName());
-		MyThread myThread2=new MyThread();
-		myThread2.run();
-		System.out.println(myThread2.getId());
-		System.out.println(myThread2.getName());
-		MyThread myThread3=new MyThread();
-		myThread3.checkAccess();
-		System.out.println(myThread3.getState());
-		System.out.println(myThread3.getContextClassLoader());
-*/
-		/*//int count = 1;
+		int count = 1;
 		List<Employee> empList = new ArrayList<>();
 		Scanner scanner = new Scanner(System.in);
 		for (int i = 1; i <= 29 ; i++) {
@@ -86,11 +32,8 @@ public class SpringBootCodexBoxJava2022Batch1Application {
 			Employee employee = new Employee(name, id, address);
 			empList.add(employee);
 		}
-		System.out.println("My total employees : " + empList.size());*/
-		sum(20,30);
-		sum=x+y;
-		System.out.println("sum of two numbers");
-		System.out.println(thread1.getState());
+		System.out.println("My total employees : " + empList.size());
+
 	}
 
 }
