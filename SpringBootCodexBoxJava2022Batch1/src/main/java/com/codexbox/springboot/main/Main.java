91e99e84f4e3b8adb94a7281f97ec3ae03ded8ef
SASIGUNTURI <sasigunturi143@gmail.com>
2022-06-15 20:00:40 +0530

    commit to threadpool  in methods

--- SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/main/Main.java
+++ SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/main/Main.java
@@ -1,15 +1,29 @@
 package com.codexbox.springboot.main;
 
-import com.codexbox.springboot.threading.Multithreading;
+import com.codexbox.springboot.threading.SynchronisedThreadpool;
+import com.codexbox.springboot.threading.Threadpool;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
     public static void main(String[] args) {
-              int n = 8;
-        for(int i=0;i<n;i++){
-            Multithreading multithreading = new Multithreading();
-            try {
-                System.out.println(multithreading.isAlive());
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
-        }}}
+
+        //        int n = 8;
+//        for(int i=0;i<n;i++){
+//            Multithreading multithreading = new Multithreading();
+//            try {
+//                System.out.println(multithreading.isAlive());
+//            } catch (Exception e) {
+//                throw new RuntimeException(e);
+//            }
+//        }
+       // for (int i = 1; i <=2;i++) {
+            Threadpool threadpool = new Threadpool();
+            ExecutorService es = Executors.newFixedThreadPool(2);
+            es.execute(threadpool);
+            es.execute(threadpool);
+            es.shutdown();
+        }
+    }
+//}//
