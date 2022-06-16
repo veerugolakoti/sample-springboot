da4bf6cfe9836515034fe4b0551fae1f68671a4b
jadduleelaganesh <jadduleelaganesh@gmail.com>
2022-06-16 10:24:47 +0530

    Executing Framework(changes are done)

--- SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/ganesh/multithreading/ExecuteFrameworkExample.java
+++ SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/ganesh/multithreading/ExecuteFrameworkExample.java
@@ -2,20 +2,14 @@ package com.codexbox.springboot.app.ganesh.multithreading;
 
 public class ExecuteFrameworkExample implements Runnable {
 
-
-
-
-
-        @Override
-        public void run () {
-            for (int i = 1; i < 10; i++) {
-                try {
-         Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-                System.out.println("This is executing framework");
-            }
-
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
+        System.out.println("This is executing framework");
     }
+}
+
