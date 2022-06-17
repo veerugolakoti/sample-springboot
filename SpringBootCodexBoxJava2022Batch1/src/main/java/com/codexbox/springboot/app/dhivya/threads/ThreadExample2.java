07f4f44a71f5a5742d1bf9b21d2e1b573c5a6edc
divya <dhivyakesavulu1999@gmail.com>
2022-06-17 18:59:25 +0530

    jdbc statements and prepared statements

--- SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/dhivya/threads/ThreadExample2.java
+++ SpringBootCodexBoxJava2022Batch1/src/main/java/com/codexbox/springboot/app/dhivya/threads/ThreadExample2.java
@@ -4,5 +4,11 @@ public class ThreadExample2 implements Runnable{
     @Override
     public void run() {
         System.out.println(" Thread");
+        try {
+            Thread.sleep(50000);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
